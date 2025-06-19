254a8d3c5dd52fba424d379757ea385764e62ebc
Marc2494 <marcandre.marchand.cem@gmail.com>
2025-06-19 17:31:22 -0400

    Delete 8. 420-BD2-ID Bases de données 2

--- 8. 420-BD2-ID Bases de données 2/Fichiers/Etape9.sql
+++
@@ -1,7 +0,0 @@
-USE MultiLocations7336
-GO
-
-SELECT c.Prenom, c.Nom, c.Telephone, COUNT(*) 'QuantiteLoue'
-FROM Clients c INNER JOIN Locations l ON
-c.ClientID = l.ClientID
-GROUP BY c.Telephone, c.Nom, c.Prenom
