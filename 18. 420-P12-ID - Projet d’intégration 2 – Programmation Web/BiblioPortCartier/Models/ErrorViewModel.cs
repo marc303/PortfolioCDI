19097a03003e04ad06e646b7940001b621d05411
marc303 <MarcAndre.Marchand.CEM@gmail.com>
2025-06-19 18:12:30 -0400

    Delete 18. 420-P12-ID - Projet d’intégration 2 – Programmation Web directory

--- 18. 420-P12-ID - Projet d’intégration 2 – Programmation Web/BiblioPortCartier/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BiblioPortCartier.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
